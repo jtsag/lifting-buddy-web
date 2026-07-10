369fc69e51a2da9e6af3dbc6abae1f0891116fcf
jtsag <tsagalisjohn@gmail.com>
2026-07-10 18:04:52 -0500

    Change firebase caching rules

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
@@ -14,6 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache()
+});
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
